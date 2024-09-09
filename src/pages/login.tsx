39aaec8323a6dae2a8b4fb97a6ef2922905b7f02
Mandeep Kumar <mk1773612@gmail.com>
2024-09-09 03:15:01 -0300

    login error fixed

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,47 +3,98 @@ import { useState } from "react"
 import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "../firebase";
-import { useLoginMutation } from "../redux/api/userAPI";
+import { getUser, useLoginMutation } from "../redux/api/userAPI";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
 import { MessageResponse } from "../types/api-types";
+import { useDispatch } from "react-redux";
+import { userExist, userNotExist } from "../redux/reducer/userReducer";
 
 
-const Login = () => {
+// const Login = () => {
 
-    const [gender, setGender] = useState("");
-    const [date, setDate] = useState("");
+//     const [gender, setGender] = useState("");
+//     const [date, setDate] = useState("");
 
-    const [login] = useLoginMutation();
+//     const [login] = useLoginMutation();
+
+    // const loginHandler = async () => {
+    //     try {
+    //         const provider = new GoogleAuthProvider();
+    //         const { user } = await signInWithPopup(auth, provider);
+
+    //         const res = await login({
+    //             name: user.displayName!,
+    //             email: user.email!,
+    //             photo: user.photoURL!,
+    //             gender: gender,
+    //             role: "user",
+    //             dob: date,
+    //             _id: user.uid,
+    //         });
+
+    //         if("data" in res){
+    //             toast.success(res.data.message);
+    //         }else{
+    //             const error = res.error as FetchBaseQueryError;
+    //             const message = (error.data as MessageResponse).message;
+    //             toast.error(message);                
+    //         }
+
+    //         console.log(user);
+
+    //     } catch (error) {
+    //         toast.error("Sign in Failed");
+    //     }
+    // };
+
+    const Login = () => {
+        const dispatch = useDispatch();
+        const [gender, setGender] = useState("");
+        const [date, setDate] = useState("");
+      
+        const [login] = useLoginMutation();
 
     const loginHandler = async () => {
         try {
-            const provider = new GoogleAuthProvider();
-            const { user } = await signInWithPopup(auth, provider);
-
-            const res = await login({
-                name: user.displayName!,
-                email: user.email!,
-                photo: user.photoURL!,
-                gender: gender,
-                role: "user",
-                dob: date,
-                _id: user.uid,
-            });
-
-            if("data" in res){
-                toast.success(res.data.message);
-            }else{
-                const error = res.error as FetchBaseQueryError;
-                const message = (error.data as MessageResponse).message;
-                toast.error(message);                
-            }
-
-            console.log(user);
-
+          const provider = new GoogleAuthProvider();
+          const { user } = await signInWithPopup(auth, provider);
+    
+          console.log({
+            name: user.displayName!,
+            email: user.email!,
+            photo: user.photoURL!,
+            gender,
+            role: "user",
+            dob: date,
+            _id: user.uid,
+          });
+    
+          const res = await login({
+            name: user.displayName!,
+            email: user.email!,
+            photo: user.photoURL!,
+            gender,
+            role: "user",
+            dob: date,
+            _id: user.uid,
+          });
+    
+          if ("data" in res) {
+            toast.success(res.data.message);
+            const data = await getUser(user.uid);
+            // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+            dispatch(userExist(data?.user!));
+          } else {
+            const error = res.error as FetchBaseQueryError;
+            const message = (error.data as MessageResponse).message;
+            toast.error(message);
+            dispatch(userNotExist());
+          }
         } catch (error) {
-            toast.error("Sign in Failed");
+          toast.error("Sign In Fail");
         }
-    };
+      };
+    
 
   return (
     <div className="login">
